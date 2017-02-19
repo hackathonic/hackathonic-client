7cc46349d123e76b293e6badf503ccbcbfef323c
Sebastian Rosik <sebastian.rosik@gmail.com>
2017-02-19 15:54:50 +0100

    Basis for the client application

---
+++ README.md
@@ -0,0 +1,1 @@
+# hackathonic-client
---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: 'app',
+  components:  {}
+}
+</script>
+
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<style lang="scss">
+</style>
